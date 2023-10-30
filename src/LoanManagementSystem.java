82df1abc51e1df53c69e8e607b9ae39ac04b8a65
kbabayevv <kamran.babayevv@gmail.com>
2023-10-30 14:38:18 +0400

    Initial commit

---
+++ src/LoanManagementSystem.java
@@ -0,0 +1,92 @@
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LoanManagementSystem {
+    public static void main(String[] args) {
+        Map<String, LoanApplication> loanApplications = new HashMap<>();
+
+        Map<String, LoanRange> banks = new HashMap<>();
+        banks.put("VTB", new LoanRange(0, 500));
+        banks.put("Bank Respublika", new LoanRange(500, 1000));
+        banks.put("Express", new LoanRange(500, 1000));
+        banks.put("Rabita", new LoanRange(1000, 2000));
+        banks.put("Azerturk", new LoanRange(1000, 2000));
+        banks.put("Xalq", new LoanRange(1000, 2000));
+        banks.put("Kapital", new LoanRange(2000, 3000));
+        banks.put("Pasha", new LoanRange(2000, 3000));
+        banks.put("Ibar", new LoanRange(2000, 3000));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your full name: ");
+        String fullName = scanner.nextLine();
+        System.out.print("Enter your birth date: ");
+        LocalDate birthDate = LocalDate.parse(scanner.nextLine());
+        System.out.print("Enter loan amount: ");
+        double loanAmount = scanner.nextDouble();
+        System.out.print("Enter loan months: ");
+        int loanMonths = scanner.nextInt();
+
+
+        if (loanAmount < 500) {
+            System.out.println("Error: Loan amount must be at least 500.");
+        } else {
+            String selectedBank = null;
+            for (Map.Entry<String, LoanRange> entry : banks.entrySet()) {
+                LoanRange range = entry.getValue();
+                if (loanAmount >= range.getMin() && loanAmount <= range.getMax()) {
+                    selectedBank = entry.getKey();
+                    break;
+                }
+            }
+
+            if (selectedBank == null) {
+                System.out.println("Error: Loan amount not in the valid range.");
+            } else {
+                String applicationKey = fullName + birthDate + loanAmount + loanMonths;
+                if (loanApplications.containsKey(applicationKey)) {
+                    System.out.println("Error: This application is already present.");
+                } else {
+                    loanApplications.put(applicationKey, new LoanApplication(fullName, birthDate, loanAmount, loanMonths, selectedBank));
+                    System.out.println("Your application confirmed.");
+                }
+            }
+        }
+    }
+}
+
+class LoanRange {
+    private double min;
+    private double max;
+
+    public LoanRange(double min, double max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double getMax() {
+        return max;
+    }
+}
+
+class LoanApplication {
+    private String fullName;
+    private LocalDate birthDate;
+    private double loanAmount;
+    private int loanMonths;
+    private String bank;
+
+    public LoanApplication(String fullName, LocalDate birthDate, double loanAmount, int loanMonths, String bank) {
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.loanAmount = loanAmount;
+        this.loanMonths = loanMonths;
+        this.bank = bank;
+    }
+}
+
